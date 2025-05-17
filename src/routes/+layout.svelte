16911adcf7b8807afa1f30caa2abdf95ccfed62c
RedHellier <redhellier@gmail.com>
2025-05-17 16:33:06 +0100

    root layout changes based on role

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 	
 	let { children } = $props();
+	const role = "admin";
 </script>
+
+{#if role === "admin"}
+	<h1>Admin Panel</h1>
+{:else if role === "user"}
+	<h1>Admin User</h1>
+
 
 {@render children()}
